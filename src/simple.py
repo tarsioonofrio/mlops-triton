5d75788dd340ea88d2ece8a09db08511f9d72428
tarsio <5504075+tarsioonofrio@users.noreply.github.com>
2020-12-31 02:51:20 -0300

    simple as updated

--- src/simple.py
+++ src/simple.py
@@ -1,16 +1,23 @@
 # https://github.com/triton-inference-server/server/issues/1989
+import numpy as np
+from PIL import Image
 
 from tritonhttpclient import InferenceServerClient, InferInput
-import numpy as np
 
+filename = "/workspace/images/mug.jpg"
 model_name = "densenet_onnx"
 
 triton_client = InferenceServerClient("localhost:8000")
 config = triton_client.get_model_config(model_name)
 
-img = np.random.rand(3, 224, 224).astype(np.float32)
+img = Image.open(filename)
+img = img.resize((224, 224), Image.ANTIALIAS)
+img = np.array(img).astype(np.float32)
+img = np.rollaxis(img, 2, 0)
+# img = np.random.rand(3, 224, 224).astype(np.float32)
 inp = InferInput(config['input'][0]['name'], img.shape, 'FP32')
 inp.set_data_from_numpy(img)
 
 for i in range(11):
     response = triton_client.infer(model_name=model_name, inputs=[inp])
+    print(response.get_output(config['input'][0]["format"]))
